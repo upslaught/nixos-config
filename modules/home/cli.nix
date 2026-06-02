a8ca52cba0be3226b5be37918d0b70a8a2fbfa14
Upslaught <upslaught@tuta.io>
2026-06-02 20:56:51 +0300

    +xdg-utils

--- modules/home/cli.nix
+++ modules/home/cli.nix
@@ -1,7 +1,4 @@
-{ pkgs, ... }:
-{
-  home.packages = [ pkgs.wl-clipboard ];
-
+{pkgs, ...}: {
   programs = {
     # i wouldn't consider this a cli but whatever
     foot.enable = true;
@@ -10,9 +7,12 @@
     eza.enable = true;
     btop.enable = true;
     fd.enable = true;
+    xdg-utils.enable = true;
     zoxide = {
       enable = true;
-      enableFishIntegration = true;
+
+      # doesn't seem to work
+      # enableFishIntegration = true;
 
       options = [
         "--cmd cd" # +50 iq
